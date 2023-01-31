bfaaaaedb815e506c439a7e66d95d07259f63d6d
Iain <iain@zora.co>
2023-01-31 12:32:50 -0500

    simulation rendering fixes

--- src/components/ProposalSimulation.tsx
+++ src/components/ProposalSimulation.tsx
@@ -23,54 +23,60 @@ export const ProposalSimulation = ({
 
   const [gasEstimate, setGasEstimate] = useState<any>();
   const [gasPrice, setGasPrice] = useState<any>();
+  const [transactionError, setTransactionError] = useState<any>();
 
   const callAggregate = useCallback(
     async (contract, transactions) => {
-      if (contract && transactions?.length > 0) {
-        const totalValue = transactions.reduce(
-          (last: typeof BigNumber, txn: Transaction) =>
-            BigNumber.from(last).add(BigNumber.from(txn.data.value)),
-          BigNumber.from("0")
-        );
-        const txns = transactions.map((transaction: Transaction) => ({
-          target: transaction.data.to,
-          allowFailure: true,
-          value: BigNumber.from(transaction.data.value).toString(),
-          callData: transaction.data.calldata,
-        }));
+      try {
+        setTransactionError(undefined);
+        if (contract && transactions?.length > 0) {
+          const totalValue = transactions.reduce(
+            (last: typeof BigNumber, txn: Transaction) =>
+              BigNumber.from(last).add(BigNumber.from(txn.data.value)),
+            BigNumber.from("0")
+          );
+          const txns = transactions.map((transaction: Transaction) => ({
+            target: transaction.data.to,
+            allowFailure: true,
+            value: BigNumber.from(transaction.data.value).toString(),
+            callData: transaction.data.calldata,
+          }));
 
-        const result = await contract.callStatic.aggregate3Value(txns, {
-          from: daoTreasuryAddress,
-          value: totalValue,
-        });
-
-        console.log({ result });
-
-        if (result.every((result) => result.success)) {
-          const gasEstimate = await provider.estimateGas({
-            to: MULTICALL_CONTRACT,
+          const result = await contract.callStatic.aggregate3Value(txns, {
             from: daoTreasuryAddress,
-            data: contract.interface.encodeFunctionData("aggregate3Value", [
-              txns,
-            ]),
             value: totalValue,
           });
-          const feeData = await provider.getFeeData();
-          console.log({
-            estimate: gasEstimate.toString(),
-            fee: feeData.maxFeePerGas.toString(),
-            price: feeData.gasPrice.toString(),
-          });
-          setGasPrice(feeData.maxFeePerGas);
-          setGasEstimate(
-            BigNumber.from(gasEstimate)
-              .mul(feeData.maxFeePerGas)
-              .mul(BigNumber.from("125").div("100"))
-          );
+
+          if (result.every((result) => result.success)) {
+            const gasEstimate = await provider.estimateGas({
+              to: MULTICALL_CONTRACT,
+              from: daoTreasuryAddress,
+              data: contract.interface.encodeFunctionData("aggregate3Value", [
+                txns,
+              ]),
+              value: totalValue,
+            });
+            const feeData = await provider.getFeeData();
+            console.log({
+              estimate: gasEstimate.toString(),
+              fee: feeData.maxFeePerGas.toString(),
+              price: feeData.gasPrice.toString(),
+            });
+            setGasPrice(
+              feeData.maxFeePerGas.div(BigNumber.from("10").pow(9)).toString()
+            );
+            setGasEstimate(
+              BigNumber.from(gasEstimate)
+                .mul(feeData.maxFeePerGas)
+                .mul(BigNumber.from("125").div("100"))
+            );
+          }
         }
+      } catch (error: any) {
+        setTransactionError(error.toString().slice(0, 100));
       }
     },
-    [setGasEstimate, setGasPrice]
+    [setGasEstimate, setGasPrice, setTransactionError]
   );
 
   useEffect(() => {
@@ -79,14 +85,22 @@ export const ProposalSimulation = ({
 
   if (gasEstimate) {
     return (
-      <div className="flex flex-between text-md">
-        <div>Estimated proposal execution gas:</div>
-        <div>
-          <span>{formatEther(gasEstimate)}</span>
-        </div>{" "}
-        <span className="text-gray-800">ETH at {gasPrice} GWEI</span>
+      <div className="flex justify-between text-lg w-full">
+        <div title="Estimated gas for proposal execution">
+          Estimated proposal execution gas:
+        </div>
+        <div className="font-light">
+          <span>{formatEther(gasEstimate).slice(0, 10)}</span>
+          <span className="text-gray-800"> ETH at {gasPrice} GWEI</span>
+        </div>
       </div>
     );
+  }
+
+  if (transactionError) {
+    <div className="text-lg">
+      Transaction simulation error: {transactionError}
+    </div>;
   }
 
   return <div>Simulating...</div>;
