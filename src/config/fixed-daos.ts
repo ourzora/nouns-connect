4471b3134d9f1e4cbd419c75d49ad7037857bb9a
Iain <iain@zora.co>
2023-02-07 16:39:48 -0500

    update daos registry

--- src/config/fixed-daos.ts
+++ src/config/fixed-daos.ts
@@ -3,7 +3,7 @@ import { CHAIN_ID } from "../utils/constants";
 const NON_BUILDER_DAOS_GOERLI = [
   {
     name: "Lilnouns DAO Testnet",
-    collectionAddress: "0x1d0030f304b542d5a5f77cc0e5945fd79ec89d8e",
+    collectionAddress: "0xbd9bd9722fde1ec321f590993f7f5961f1bd0d06",
     governorAddress: "0x4f49457d0ddd610b451686aeb17de9562094cd00",
     treasuryAddress: "0xd82c7DC502cbF88cFc5F0821BC514BBA88d70513",
     proposalLink: "https://lilnouns.wtf/vote/PROPOSAL_ID",
@@ -11,9 +11,9 @@ const NON_BUILDER_DAOS_GOERLI = [
   },
   {
     name: "Nouns DAO Testnet",
-    collectionAddress: "0xbd9bd9722fde1ec321f590993f7f5961f1bd0d06",
+    collectionAddress: "0x1d0030f304b542d5a5f77cc0e5945fd79ec89d8e",
     governorAddress: "0xd08faceb444dbb6b063a51c2ddfb564fa0f8dce0",
-    treasuryAddress: "0x2f12ABA664E6D2b4DDD264E2a175d29703836AaE",
+    treasuryAddress: "0x2f12aba664e6d2b4ddd264e2a175d29703836aae",
     proposalLink: "https://master--nouns-testnet.netlify.app/vote/PROPOSAL_ID",
     site: "https://nouns.wtf/",
   },
